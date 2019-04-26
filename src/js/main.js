95ef30c220437fbf3e756591b2b71f0e19be3457
Ana Jovic <suton15@gmail.com>
2019-04-26 19:02:52 +0200

    js

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,143 @@
-jQuery(document).ready(function() {
-	jQuery('.toggle-nav').click(function(e) {
+// navigation
+jQuery(document).ready(function () {
+	jQuery('.toggle-nav').click(function (e) {
 		jQuery(this).toggleClass('active');
 		jQuery('.menu ul').toggleClass('active');
-
 		e.preventDefault();
 	});
 });
+
+// shop and cart
+let main = (function () {
+
+	function init() {
+		$catalog.on('click', buyProduct);
+		$catalog.on('input', calculateTotal);
+		$shoppingCartContent.on('click', removeProduct);
+		$clearCartBtn.on('click', clearCart);
+		$(document).on('DOMContentLoaded', clearCart);
+	}
+	const $catalog = $('#catalog'),
+		$shoppingCartContent = $('#cart-content tbody'),
+		$clearCartBtn = $('#clear-cart');
+	let cart = 0;
+	function buyProduct(e) {
+		e.preventDefault();
+		if (e.target.classList.contains('add-to-cart')) {
+			const product = e.target.parentElement;
+			const quantity = e.target.parentElement.querySelector('.productQuantity');
+			if (quantity.value < 0 || quantity.value === '') {
+				quantity.style.border = '1px solid red';
+				quantity.value = '';
+				e.target.parentElement.querySelector('.productSum').value = '';
+			} else {
+				getProducInfo(product);
+			}
+		}
+	}
+
+	function getProducInfo(product) {
+		const productInfo = {
+			image: product.querySelector('img').src,
+			title: product.querySelector('h3').textContent,
+			price: product.querySelector('.productPrice').value,
+			quantity: parseInt(product.querySelector('.productQuantity').value),
+			productSum: product.querySelector('.productSum').value,
+			cart_id: cart
+		}
+		addIntoCart(productInfo);
+	}
+
+	function calculateTotal(e) {
+		let quantity = parseInt(e.target.value),
+			price = parseInt(e.target.parentElement.parentElement.querySelector('.productPrice').value),
+			productSum = e.target.parentElement.parentElement.querySelector('.productSum');
+		e.target.style.border = '2px inset white';
+		productSum.value = price * quantity;
+	}
+
+	function addIntoCart(product) {
+		const row = $('<tr></tr>');
+		const { title, price, quantity, image, productSum } = product;
+		$(`    <td>
+				 <img src="${image}" width=50px>
+			</td>
+			<td>${title}</td>
+			<td>${price}</td>
+			<td>${quantity}</td>
+			<td>${productSum}</td>
+			<td>
+				<a href="#" class="remove" id="${cart++}">X</a>
+			</td>
+	`).appendTo(row);
+
+		row.appendTo($shoppingCartContent);
+		saveIntoStorage(product)
+	}
+
+	function saveTotalIntoStorage(product) {
+		let total = 0;
+		product.forEach(element => {
+			total = parseInt(total) + parseInt(element.productSum);
+		});
+		document.getElementById('total').innerText = `To Pay: ${total} RSD`;
+		sessionStorage.setItem('total', JSON.stringify(total));
+	}
+
+	function saveIntoStorage(product) {
+		let products = getProductFromStorage();
+		products.push(product);
+		saveTotalIntoStorage(products);
+		sessionStorage.setItem('products', JSON.stringify(products));
+	}
+
+	function getProductFromStorage() {
+		let products;
+		if (sessionStorage.getItem('products') === null) {
+			products = [];
+		} else {
+			products = JSON.parse(sessionStorage.getItem('products'));
+		}
+		return products;
+	}
+
+	function removeProduct(e) {
+		let product, productId;
+		if (e.target.classList.contains('remove')) {
+			e.target.parentElement.parentElement.remove();
+			product = e.target.parentElement.parentElement;
+			productId = product.querySelector('a').getAttribute('id');
+			console.log(productId);
+		}
+		removeProductFromSessionStorage(productId);
+	}
+
+	function removeProductFromSessionStorage(id) {
+		let productsSS = getProductFromStorage();
+		productsSS.forEach(function (productSS, index) {
+			if (productSS.cart_id == id) {
+				productsSS.splice(index, 1);
+			}
+		})
+
+		sessionStorage.setItem('products', JSON.stringify(productsSS));
+		saveTotalIntoStorage(productsSS);
+	}
+
+	function clearCart() {
+		clearSessionStorage();
+		$shoppingCartContent.html('');
+		$('#total').html('');
+
+	}
+
+	function clearSessionStorage() {
+		sessionStorage.clear();
+		cart = 1;
+	}
+
+	return {
+		init
+	}
+})();
+export default main
--- src/js/shop.js
+++ src/js/shop.js
@@ -1,52 +1,41 @@
-let product = (function(){
- 
+let product = (function() {
     const arr = [];
     let $catalog = $('#catalog');
-    
-    class Product {
-        constructor(name,price,quantity,img){
-            this.name = name,
-            this.price = price,
-            this.quantity = quantity,
-            this.img = img
-        }
-    }
-    
-    function addToCatalog(name,price,quantity,img){
-        const product = new Product(name,price,quantity,img);
-        arr.push(product);
-        createCatalog();
-    }
- 
-    function createCatalog(){
-        $catalog.html('');
-        arr.forEach(function(element) {
-        const {img,name,price} = element;
-    
-         let $product = $(`<div class = 'product'>
-                            <img src = ${img} alt='${name}-img'>
-                            <div><h3>${name}</h3></div>
-                            <div>price<input type = 'text' class = 'productPrice' value = '${price}' readonly></div>
-                            <div>quantity: <input type = 'number' class = 'productQuantity' min = '0'></div>
-                            <div>total price: <input type = 'number' class = 'productSum' readonly></div>
-                            <button type = 'submit' class = 'add-to-cart' id = '${name}'>add to cart</button>
-                        </div>`);
-        $product.appendTo($catalog);
-    });   
-    }
-        return {
-            addToCatalog
-        }
+  
+      class Product {
+          constructor(name,price,quantity,img){
+              this.name = name,
+              this.price = price,
+              this.quantity = quantity,
+              this.img = img
+          }
+      }
+      
+      function addToCatalog(name,price,quantity,img){
+          const product = new Product(name,price,quantity,img);
+          arr.push(product);
+          createCatalog();
+      }
    
-    })();
-    product.addToCatalog('Beli Pasulj(200g)',180,100,'images/Product/pasuljb.jpg');
-    product.addToCatalog('Crni Pasulj(100g)',230,100,'images/Product/pasuljcrn.jpg');
-    product.addToCatalog('Crveni Pasulj(100g)',120,100,'images/Product/pasuljc.jpg');
-    product.addToCatalog('Žuti Pasulj(200g)',190,100,'images/Product/pasuljz.jpg');
-    product.addToCatalog('Šareni Pasulj(200g)',175,100,'images/Product/pasuljs.jpg');
-    product.addToCatalog('Cacarica(100g)',120,100,'images/Product/cacarica.jpg');
-    product.addToCatalog('Luk Crni(seme)',320,100,'images/Product/lukcrn.jpg');
-    product.addToCatalog('Luk Crveni(seme)',120,100,'images/Product/lukcrven.jpg');
-    product.addToCatalog('Beli Luk(seme)',190,100,'images/Product/lukbeo.jpg');
- 
-   
+      function createCatalog(){
+          $catalog.html('');
+          arr.forEach(function(element) {
+          const {img,name,price} = element;
+      
+           let $product = $(`<div class = 'product'>
+                              <img src = ${img} alt='${name}-img'>
+                              <div><h3>${name}</h3></div>
+                              <div>price<input type = 'text' class = 'productPrice' value = '${price}' readonly></div>
+                              <div>quantity: <input type = 'number' class = 'productQuantity' min = '0'></div>
+                              <div>total price: <input type = 'number' class = 'productSum' readonly></div>
+                              <button type = 'submit' class = 'add-to-cart' id = '${name}'>add to cart</button>
+                          </div>`);
+          $product.appendTo($catalog);
+      });   
+  }
+          return {
+              addToCatalog
+          }
+     
+  })();
+  export default product;
